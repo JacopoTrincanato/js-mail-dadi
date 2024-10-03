8ee4a36a38c45aa6b9b06822f0d6c307b0fa28b2
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-03 16:04:54 +0200

    create dadi program for show the winner

--- dadi/main.js
+++ dadi/main.js
@@ -6,3 +6,15 @@ console.log(player);
 //creo una variabile per il computer
 let computer = Math.floor(Math.random() * 6) + 1;
 console.log(computer);
+
+//creo il programma per stabilire il vincitore tra il giocatore e il computer
+if (player > computer) {
+    console.log('Hai vinto player');
+    
+}else if (computer > player) {
+    console.log('il computer ti ha battuto');
+    
+}else {
+    console.log('Pareggio');
+    
+}
