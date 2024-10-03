592d784720a1934867cb097df652fa9d72b6ff14
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-03 15:17:14 +0200

    try to create a program to verify if a person can log or not

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 console.log('ciao');
 
-
+//creo il prompt per inserire la mail
 let insert_email = prompt('Inserisci la tua mail');
+console.log(insert_email);
 
+
+//creo l'array users
 let users = ['Jacopo', 'Emanuele', 'Luca', 'Claudia', 'Giulia'];
+console.log(users);
 
-let emails = ['Jacopo@gmail.com', 'Emanuele@gmail.com', 'Luca@gmail.com', 'Claudia@gmail.com', 'Giulia@gmail.com']
+
+//creo l'array emails
+let emails = ['Jacopo@gmail.com', 'Emanuele@gmail.com', 'Luca@gmail.com', 'Claudia@gmail.com', 'Giulia@gmail.com'];
+console.log(emails);
+
+//creo il programma per verificare se posso accedere oppure no
+if (insert_email == emails.indexOf()) {
+    console.log('Puoi accedere');
+    
+}else {
+    console.log('accesso negato');
+    
+}
