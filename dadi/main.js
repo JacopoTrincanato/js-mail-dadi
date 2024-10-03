56420d3ebce91591cb1c86b7e410d8a0b7f30ed6
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-03 15:57:30 +0200

    log dadi variables player and computer

--- dadi/main.js
+++ dadi/main.js
@@ -1,5 +1,8 @@
 //creo una variabile per il giocatore
 let player = Math.floor(Math.random() * 6) + 1;
+console.log(player);
+
 
 //creo una variabile per il computer
 let computer = Math.floor(Math.random() * 6) + 1;
+console.log(computer);
