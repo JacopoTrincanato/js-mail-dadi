f7756e8c025c3ac948e797bf6ba8acfc9a55b597
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-03 14:57:20 +0200

    create variable email, names and emails  array

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('ciao');
+
+
+let insert_email = prompt('Inserisci la tua mail');
+
+let users = ['Jacopo', 'Emanuele', 'Luca', 'Claudia', 'Giulia'];
+
+let emails = ['Jacopo@gmail.com', 'Emanuele@gmail.com', 'Luca@gmail.com', 'Claudia@gmail.com', 'Giulia@gmail.com']
